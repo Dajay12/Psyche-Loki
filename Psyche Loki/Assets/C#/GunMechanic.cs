ab74aed194a8f9c02d54168011061b076e26cc43
DAJAY <105812454+Dajay12@users.noreply.github.com>
2022-06-26 23:19:40 +0100

    Tap 2 Aim
    feature is still unfinished

--- Psyche Loki/Assets/C#/GunMechanic.cs
+++ Psyche Loki/Assets/C#/GunMechanic.cs
@@ -15,7 +15,7 @@ public class GunMechanic : MonoBehaviour
     [Range(0, 6)]
     public int sphereRange;
 
-    [SerializeField] bool touch2Aim;
+    [SerializeField] bool tap2Aim;
 
     private float fireRate = 2.5f;
     private float nextTimeToFire = 0;
@@ -34,15 +34,21 @@ public class GunMechanic : MonoBehaviour
 
     void Update()
     {
-        /*if (Input.touchCount > 0)
+        for (int i = 0; i < Input.touchCount; i++)
         {
-            Input.GetTouch(0);
-            //Touch.
-            print("Touch Phone");
-        }*/
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.touches[i].position);
 
-        //Raycasting
+            RaycastHit2D tap = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.zero), 10f);
+            if(tap)
+            {
+                Debug.Log("You've tapped " + tap.transform.name);
+            }
 
+
+
+            //Debug.Log(touchPos);
+            Debug.DrawLine(transform.position, touchPos, Color.green);
+        }
 
         //Aiming
         Vector2 direction = target.position - transform.position;
@@ -53,48 +59,58 @@ public class GunMechanic : MonoBehaviour
         if (target == null)
             return;
 
-
     }
 
     void Aim()
     {
-        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-        float shortestDistance = Mathf.Infinity;
-        GameObject nearestEnemy = null;
-
-        foreach (GameObject enemy in enemies)
+        if (tap2Aim != true)
         {
-            float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
-            if (distanceToEnemy < shortestDistance)
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            float shortestDistance = Mathf.Infinity;
+            GameObject nearestEnemy = null;
+
+            foreach (GameObject enemy in enemies)
             {
-                shortestDistance = distanceToEnemy;
-                nearestEnemy = enemy;
+                float distanceToEnemy = Vector2.Distance(transform.position, enemy.transform.position);
+                if (distanceToEnemy < shortestDistance)
+                {
+                    shortestDistance = distanceToEnemy;
+                    nearestEnemy = enemy;
+                }
+            }
+
+            if (nearestEnemy != null && shortestDistance <= sphereRange)
+            {
+                target = nearestEnemy.transform;
+            }
+            else
+            {
+                target = null;
             }
         }
+        else if (tap2Aim == true)
+        {
+            Debug.Log("stop touch my face");
+        }
 
-        if (nearestEnemy != null && shortestDistance <= sphereRange)
-        {
-            target = nearestEnemy.transform;
-        }
-        else
-        {
-            target = null;
-        }
     }
+
     public void Shoot()
     {
         if (Time.time >= nextTimeToFire)
         {
             //Greater the firerate fast the gun shoot and vice versa.
-            nextTimeToFire = Time.time + 1f/fireRate;
+            nextTimeToFire = Time.time + 1f / fireRate;
 
             Debug.DrawRay(firePoint.transform.position, transform.TransformDirection(Vector2.right) * 6f, Color.red);
             if (refToHealth.currentHealth > 0)
             {
+                //Shooting bullets GameObject 
                 //GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
                 //Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
                 //rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
 
+                //Raycasting bullets
                 RaycastHit2D hit = Physics2D.Raycast(firePoint.transform.position, transform.TransformDirection(Vector2.right), 6f);
                 if (hit)
                 {
@@ -109,7 +125,6 @@ public class GunMechanic : MonoBehaviour
             }
         }
     }
-
 
     void OnDrawGizmosSelected()
     {
